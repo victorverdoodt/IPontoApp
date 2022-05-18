84058e073b4fd97962cd9cb6c0d1d941fbaee2da
Victor Wesley <ti3@qualitashumanus.com.br>
2022-05-18 11:45:31 -0300

    Adicionado banco no diretorio de arquivos

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key_facerecognition'
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///banco.sqlite'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
